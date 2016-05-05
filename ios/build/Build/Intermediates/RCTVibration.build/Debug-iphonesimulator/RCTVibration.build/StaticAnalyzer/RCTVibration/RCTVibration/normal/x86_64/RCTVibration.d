17ac785a0e5987da26536544e97c57e583ba8996
Jeremy Pagley <jeremyjkp@gmail.com>
2016-05-05 16:14:21 -0400

    Removal of Firebase integration CelebrationORL/CelebrationOrlApp/Issues#1

--- ios/build/Build/Intermediates/RCTVibration.build/Debug-iphonesimulator/RCTVibration.build/StaticAnalyzer/RCTVibration/RCTVibration/normal/x86_64/RCTVibration.d
+++ ios/build/Build/Intermediates/RCTVibration.build/Debug-iphonesimulator/RCTVibration.build/StaticAnalyzer/RCTVibration/RCTVibration/normal/x86_64/RCTVibration.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationApp/node_modules/react-native/Libraries/Vibration/RCTVibration.m \
-  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationApp/node_modules/react-native/Libraries/Vibration/RCTVibration.h \
+  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationOrlApp/node_modules/react-native/Libraries/Vibration/RCTVibration.m \
+  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationOrlApp/node_modules/react-native/Libraries/Vibration/RCTVibration.h \
   ../../React/Base/RCTBridgeModule.h ../../React/Base/RCTDefines.h
--- ios/build/Build/Intermediates/RCTWebSocket.build/Debug-iphonesimulator/RCTWebSocket.build/Objects-normal/x86_64/RCTSRWebSocket.d
+++ ios/build/Build/Intermediates/RCTWebSocket.build/Debug-iphonesimulator/RCTWebSocket.build/Objects-normal/x86_64/RCTSRWebSocket.d
@@ -1,5 +1,3 @@
 dependencies: \
-  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationApp/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.m \
-  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationApp/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.h \
-  ../../React/Base/RCTAssert.h ../../React/Base/RCTDefines.h \
-  ../../React/Base/RCTLog.h
+  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationOrlApp/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.m \
+  /Users/jeremypagley/Desktop/Code\ Projects/CelebrationOrlApp/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.h
