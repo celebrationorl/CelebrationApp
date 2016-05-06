6d563d6a82166a31401cff5ae8417fdf3e05f172
Jeremy Pagley <jeremyjkp@gmail.com>
2016-05-05 23:28:42 -0400

    Adding tabBar func with react-native-tabs

--- scenes/Home.js
+++ scenes/Home.js
@@ -8,6 +8,7 @@ import React, {
 
 import ButtonStyles from '../styles/buttons';
 import HomeStyles from '../styles/homeStyles';
+import Tabs from 'react-native-tabs';
 
 class Home extends Component {
 
@@ -26,33 +27,51 @@ class Home extends Component {
 
     return (
       <View style={HomeStyles.container}>
-        <Image
-          style={HomeStyles.bgImg}
-          source={require('../assets/img/celebration_orl_background_pp.png')}>
-          <Image
-            style={HomeStyles.logo}
-            source={require('../assets/img/celebration_logo.png')}
-          />
-          <Text style={HomeStyles.title}>
-            {payload.title}
-          </Text>
-          <Text style={HomeStyles.body}>
-            {payload.body}
-          </Text>
-          <TouchableHighlight
-            onPress={this.props.onSceneSwitch}
-            style={HomeStyles.button}>
-            <Text style={HomeStyles.buttonText}>About</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={this.props.onSceneInstagram}
-            style={HomeStyles.button}>
-            <Text style={HomeStyles.buttonText}>Instagram</Text>
-          </TouchableHighlight>
-        </Image>
+        <Text
+          style={HomeStyles.title}
+        >
+          Welcome to The Celebration Church App
+        </Text>
+        <Tabs
+          selected={'first'}
+          style={{backgroundColor: 'white'}}
+          selectedStyle={{color: 'lightgreen'}}
+          onSelect={el => console.log(el)}
+        >
+          <Text name="first">First</Text>
+          <Text name="second">Second</Text>
+        </Tabs>
       </View>
     );
   }
 }
+
+// TODO: Remove below code on confirm of no further need
+/*
+<Image
+  style={HomeStyles.bgImg}
+  source={require('../assets/img/celebration_orl_background_pp.png')}>
+  <Image
+    style={HomeStyles.logo}
+    source={require('../assets/img/celebration_logo.png')}
+  />
+  <Text style={HomeStyles.title}>
+    {payload.title}
+  </Text>
+  <Text style={HomeStyles.body}>
+    {payload.body}
+  </Text>
+  <TouchableHighlight
+    onPress={this.props.onSceneSwitch}
+    style={HomeStyles.button}>
+    <Text style={HomeStyles.buttonText}>About</Text>
+  </TouchableHighlight>
+  <TouchableHighlight
+    onPress={this.props.onSceneInstagram}
+    style={HomeStyles.button}>
+    <Text style={HomeStyles.buttonText}>Instagram</Text>
+  </TouchableHighlight>
+</Image>
+*/
 
 export default Home;
--- styles/homeStyles.js
+++ styles/homeStyles.js
@@ -2,34 +2,42 @@ import React from 'react-native';
 
 const HomeStyles = React.StyleSheet.create({
 
+  // container: {
+  //   backgroundColor: 'white'
+  // },
   container: {
-    backgroundColor: 'white'
-  },
-
-  bgImg: {
-    resizeMode: 'cover',
     flex: 1,
-    alignSelf: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
 
-  logo: {
-    marginBottom: 40,
-    marginTop: 75,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    height: 240,
-    width: 300
-  },
+  // TODO: Remove below code on confirm of no further need
+  // bgImg: {
+  //   resizeMode: 'cover',
+  //   flex: 1,
+  //   alignSelf: 'center',
+  //   justifyContent: 'flex-start',
+  // },
+
+  // logo: {
+  //   marginBottom: 40,
+  //   marginTop: 75,
+  //   alignSelf: 'center',
+  //   justifyContent: 'center',
+  //   height: 240,
+  //   width: 300
+  // },
 
   title: {
     marginBottom: 10,
+    textAlign: 'center',
     fontSize: 24,
     fontWeight: '600',
     justifyContent: 'center',
     alignSelf: 'center',
     backgroundColor: 'transparent',
-    color: 'white'
+    color: 'lightgrey'
   },
 
   body: {
