6511d9a7814358e4b98825573e0305bf35830cad
Jeremy Pagley <jeremyjkp@gmail.com>
2016-05-24 21:59:52 -0400

    Fill About scene in More scene Issue#5

--- styles/aboutStyles.js
+++ styles/aboutStyles.js
@@ -1,37 +1,81 @@
 import React, { Dimensions } from 'react-native';
 var { width } = Dimensions.get('window');
-import { titleColor } from './moreStyles';
+import { black } from './colorStyles';
 
 const AboutStyles = React.StyleSheet.create({
 
   container: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent: 'center',
     alignItems: 'center',
     width: width
   },
 
   title: {
+    marginBottom: 15,
     fontFamily: 'Avenir',
-    marginBottom: 10,
     textAlign: 'center',
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: '600',
     alignSelf: 'center',
-    color: titleColor
+    color: black
   },
 
   body: {
+    paddingRight: 20,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+
+  scrollView: {
+    backgroundColor: 'transparent',
+    height: 300,
+  },
+
+  primaryTextWrapper: {
+    borderBottomWidth: 2,
+    borderBottomColor: 'lightgrey'
+  },
+
+  primaryText: {
+    paddingBottom: 10,
     fontFamily: 'Avenir',
-    marginRight: 10,
-    marginLeft: 10,
-    marginBottom: 10,
+    fontSize: 18,
+    fontWeight: '800',
+    color: black,
+    alignSelf: 'center',
+  },
+
+  subPrimaryText: {
+    paddingTop: 20,
+    fontFamily: 'Avenir',
+    fontSize: 18,
+    fontWeight: '600',
+    color: black,
+    alignSelf: 'flex-start',
+    opacity: 0.94
+  },
+
+  secondaryText: {
+    paddingTop: 12,
+    fontFamily: 'Avenir',
     fontSize: 16,
     fontWeight: '400',
-    color: titleColor,
+    color: black,
     alignSelf: 'center',
     opacity: 0.85
+  },
+
+  logoWrapper: {
+    alignSelf: 'center'
+  },
+
+  logo: {
+    marginTop: 15,
+    marginBottom: 0,
+    height: 128,
+    width: 128
   }
 
 });
